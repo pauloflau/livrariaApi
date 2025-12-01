4a897777cdda0d2e2c4ff63b492956c46b6cb69e
pauloflau <pauloflau@gmail.com>
2025-12-01 17:25:12 -0300

    persistindo dados

--- src/main/java/com/jmp/paulo/livrariaApi/LivrariaApiApplication.java
+++ src/main/java/com/jmp/paulo/livrariaApi/LivrariaApiApplication.java
@@ -1,7 +1,14 @@
 package com.jmp.paulo.livrariaApi;
 
+import java.time.LocalDate;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.jmp.paulo.livrariaApi.entities.Autor;
+import com.jmp.paulo.livrariaApi.repositories.AutorRepository;
 
 @SpringBootApplication
 public class LivrariaApiApplication {
@@ -9,5 +16,16 @@ public class LivrariaApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LivrariaApiApplication.class, args);
 	}
-
+	 @Bean
+	 CommandLineRunner run(AutorRepository autorRepository) {
+		return args -> {
+			Autor autor = new Autor();
+			autor.setNome("Paulo");
+			autor.setNacionalidade("Brasileira");
+			autor.setDataNascimento(LocalDate.of(1983, 2, 22));
+		
+			Autor autorSalvo = autorRepository.save(autor);
+			System.out.println(autorSalvo);
+		};
+	  }
 }
--- src/main/java/com/jmp/paulo/livrariaApi/entities/Autor.java
+++ src/main/java/com/jmp/paulo/livrariaApi/entities/Autor.java
@@ -2,6 +2,7 @@ package com.jmp.paulo.livrariaApi.entities;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
@@ -11,13 +12,11 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
 @Table(name="tb_autor")
-@Getter
-@Setter
+@Data
 public class Autor {
 
 	@Id
@@ -35,4 +34,79 @@ public class Autor {
 	
 	@OneToMany(mappedBy = "autor")
 	private List<Livro> livros;
+	
+	public Autor() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public Autor(UUID id, String nome, LocalDate dataNascimento, String nacionalidade) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.dataNascimento = dataNascimento;
+		this.nacionalidade = nacionalidade;
+	}
+
+
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+
+	public List<Livro> getLivros() {
+		return livros;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Autor other = (Autor) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 }
