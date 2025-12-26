48e410e736d076b85a3959355f5d8b9f15543971
pauloflau <pauloflau@gmail.com>
2025-12-26 11:55:47 -0300

    criado o mapper e o controller de usuario

---
+++ src/main/java/com/jmp/paulo/livrariaApi/Mapper/MapperUsuario.java
@@ -0,0 +1,26 @@
+package com.jmp.paulo.livrariaApi.Mapper;
+
+import com.jmp.paulo.livrariaApi.dto.UsuarioDto;
+import com.jmp.paulo.livrariaApi.entities.Usuario;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class MapperUsuario {
+
+	public Usuario dtoToUsuario(UsuarioDto dto) {
+		Usuario usuario = new Usuario();
+		usuario.setLogin(dto.getLogin());
+		usuario.setSenha(dto.getSenha());
+		usuario.setRoles(dto.getRoles());
+		return usuario;
+	}
+	
+	public UsuarioDto usuarioToDto(Usuario usuario) {
+		UsuarioDto dto = new UsuarioDto();
+		dto.setLogin(usuario.getLogin());
+		dto.setSenha(usuario.getSenha());
+		dto.setRoles(usuario.getRoles());
+		return dto;
+	}
+}
---
+++ src/main/java/com/jmp/paulo/livrariaApi/controller/UsuarioController.java
@@ -0,0 +1,26 @@
+package com.jmp.paulo.livrariaApi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jmp.paulo.livrariaApi.Mapper.MapperUsuario;
+import com.jmp.paulo.livrariaApi.dto.UsuarioDto;
+import com.jmp.paulo.livrariaApi.entities.Usuario;
+import com.jmp.paulo.livrariaApi.services.UsuarioService;
+
+@RestController
+@RequestMapping("usuarios")
+public class UsuarioController {
+	private UsuarioService service;
+	
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public void salvar(@RequestBody UsuarioDto dto) {
+		 Usuario usuario = MapperUsuario.dtoToUsuario(dto);
+		 service.salvar(usuario);
+	}
+}
